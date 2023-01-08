01badba2e24d5b0c46dfc161cb9ffb3bc6af9283
admin <mmccraw98@gwu.edu>
2023-01-08 16:29:17 -0500

    created sim class to handle soft contact simulations

--- afm/data_io.py
+++ afm/data_io.py
@@ -12,6 +12,35 @@ from copy import deepcopy
 from mlinsights.mlmodel import KMeansL1L2
 
 
+def next_path(path_pattern):
+    """
+    https://stackoverflow.com/questions/17984809/how-do-i-create-an-incrementing-filename-in-python
+    Finds the next free path in an sequentially named list of files
+
+    e.g. path_pattern = 'file-%s.txt':
+
+    file-1.txt
+    file-2.txt
+    file-3.txt
+
+    Runs in log(n) time where n is the number of existing files in sequence
+    """
+    i = 1
+
+    # First do an exponential search
+    while os.path.exists(path_pattern % i):
+        i = i * 2
+
+    # Result lies somewhere in the interval (i/2..i]
+    # We call this interval (a..b] and narrow it down until a + 1 = b
+    a, b = (i // 2, i)
+    while a + 1 < b:
+        c = (a + b) // 2 # interval midpoint
+        a, b = (c, b) if os.path.exists(path_pattern % c) else (a, c)
+
+    return path_pattern % b
+
+
 def get_files(directory, req_ext=None):
     '''
     gets all the files in the given directory
--- afm/sim.py
+++ afm/sim.py
@@ -1,13 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import quadrature, quad, RK45
-from scipy.special import ellipk, ellipkm1, jv
-from scipy.optimize import curve_fit
-from afm.data_io import get_files
-import sympy as sp
+from scipy.special import ellipk, jv
+from afm.data_io import next_path
 import pandas as pd
 import pickle
-from afm.visco import mdft, get_r
 
 
 def In_L(r_n, dr):
@@ -103,34 +98,21 @@ def rk4(state, dt, rhs_func, *args):
 
 
 class sim_rajabifar_1():
-    def __init__(self, E0, viscoelastic, Einf, Tau, v0, h0, R, force_target, depth_ratio_target, nr=1000,
-                 dr=1.5e-9, dt=1e-4, nt=1e6):
+    def __init__(self, v0, h0, R, nr=1000, dr=1.5e-9, dt=1e-4, nt=1e6):
         '''
         create an instance of Rajabifar's 'exact' approach to solving the Attard equation as outlined here:
         Dynamic AFM on Viscoelastic Polymer Samples with Surface Forces - Bahram Rajabifar
-        :param E0: (float) normalized instantaneous modulus (Pa) E0 / (1 - v^2)
-        :param viscoelastic: (bool) whether sample is viscoelastic (SLS) or elastic (w/ modulus E0)
-        :param Einf: (float) normalized equilibrium modulus (Pa) Einf / (1 - v^2)
-        :param Tau: (float) relaxation time (s)
         :param v0: (float) directional velocity of the indenter (indenter follows linear path in time) (m/s)
         :param h0: (float) initial position of indenter (m)
         :param R: (float) radius of indenter (m)
-        :param force_target: (float) target force (N)
-        :param depth_ratio_target: (float) ratio of indenter radius to indent into sample
         :param nr: (int) number of points in the radial discretization
         :param dr: (float) radial discretization (m)
         :param dt: (float) temporal discretization (s)
         :param nt: (int) number of points in the temporal discretization
         '''
-        self.E0 = E0
-        self.viscoelastic = viscoelastic
-        self.Einf = Einf
-        self.Tau = Tau
         self.v0 = v0
         self.h0 = h0
         self.R = R
-        self.force_target = force_target
-        self.pos_target = - depth_ratio_target * R
         self.nr = nr
         self.dr = dr
         self.dt = dt
@@ -139,25 +121,87 @@ class sim_rajabifar_1():
         self.r = np.linspace(1, self.nr, self.nr) * self.dr
         self.k_ij = np.array([K(r_, self.r, self.dr) for r_ in self.r])  # calculate integrating kernel
         self.I = np.eye(self.r.size)  # identity matrix
-        # set loggers
-        self.force = np.zeros(nt)
-        self.separation = np.zeros(nt)
-        self.tip_pos = np.zeros(nt)
-        self.time = np.zeros(nt)
+        self.force = None
+        self.separation = None
+        self.tip_pos = None
+        self.time = None
+        self.inden = None
+        self.force_rep = None
 
-    def rhs(self, state):
+    def rhs_vdw_sls(self, state, E0, Einf, Tau, viscoelastic):
         '''
         calculate the time derivative of the state vector (u, h0) assuming that h0(t)=v0*t
+        works via inversion of J_ij * u_dot_i = b_i
         :param state: (numpy array) state vector (u sample deformation, h0 indenter position)
         :return: (numpy array) derivative of state vector (u_dot, v0)
         '''
         u, h0 = state  # unpack state vector
         h = h_calc(h0, u, f_sphere, self.r, self.R)  # calculate the separation for the spherical indenter
         p, p_h = p_vdw(h)  # calculate the vdW pressure
-        J_ij = 1 / self.E0 * self.k_ij * self.dr * p_h * self.r - self.I
-        b_i = 1 / self.E0 * self.dr * self.v0 * (p_h * self.r) @ self.k_ij
-        if self.viscoelastic:
-            u_inf = - 1 / self.Einf * self.dr * (p * self.r) @ self.k_ij  # skip if elastic
-            b_i += 1 / self.Tau * (u - u_inf)  # skip if elastic
-        u_dot = np.linalg.solve(J_ij, b_i)
+        J_ij = 1 / E0 * self.k_ij * self.dr * p_h * self.r - self.I  # calculate the LHS matrix J_ij
+        b_i = 1 / E0 * self.dr * self.v0 * (p_h * self.r) @ self.k_ij  # calculate the RHS vector b_i
+        if viscoelastic:  # introduce viscoelastic relaxation of SLS material
+            u_inf = - 1 / Einf * self.dr * (p * self.r) @ self.k_ij  # skip if elastic
+            b_i += 1 / Tau * (u - u_inf)  # skip if elastic
+        u_dot = np.linalg.solve(J_ij, b_i)  # invert governing matrix eqn to find u_dot_i directly
         return np.array([u_dot, self.v0], dtype='object')
+
+    def simulate(self, E0, Einf, Tau, viscoelastic, force_target, depth_ratio_target=0.1):
+        '''
+        :param E0: (float) normalized instantaneous modulus (Pa) E0 / (1 - v^2)
+        :param Einf: (float) normalized equilibrium modulus (Pa) Einf / (1 - v^2)
+        :param Tau: (float) relaxation time (s)
+        :param viscoelastic: (bool) whether sample is viscoelastic (SLS) or elastic (w/ modulus E0)
+        :param force_target: (float) target force (N)
+        :param depth_ratio_target: (float) ratio of indenter radius to indent into sample
+        :return:
+        '''
+        pos_target = - depth_ratio_target * self.R  # calculate position target for iteration
+
+        # set loggers
+        self.force = np.zeros(self.nt)
+        self.separation = np.zeros(self.nt)
+        self.tip_pos = np.zeros(self.nt)
+        self.time = np.zeros(self.nt)
+
+        h0 = self.h0
+        u = np.zeros(self.r.shape)
+        state = np.array([u, h0], dtype='object')
+        print(' % |  z_tip  |  z_target |  force  |  force_target')
+        for n in range(self.nt):
+            state = rk4(state, self.dt, self.rhs_vdw_sls, self.r, self.dr, self.R, self.k_ij,
+                        E0, Einf, Tau, viscoelastic)
+            u, h0 = state
+            h = h_calc(h0, u, f_sphere, self.r, self.R)
+            p, p_h = p_vdw(h)
+            self.force[n] = (2 * np.pi * p @ self.r * self.dr)
+            self.separation[n] = h[0]
+            self.tip_pos[n] = h0
+            self.time[n] = n * self.dt
+            if n % (0.01 * self.nt) == 0:
+                print('{} | {:.1f} nm | {:.1f} nm | {:.1f} nN | {:.1f} nN'.format(n / self.nt,
+                                                                                  self.tip_pos[n] * 1e9, pos_target * 1e9,
+                                                                                  self.force[n] * 1e9, force_target * 1e9))
+            if self.force[n] > force_target or self.tip_pos[n] < pos_target:
+                force = self.force[:n]
+                separation = self.separation[:n]
+                tip_pos = self.tip_pos[:n]
+                time = self.time[:n]
+                break
+        print('done!')
+        self.inden = np.zeros(time.size)
+        contact_index = np.argmin(force)
+        self.inden[contact_index:] = tip_pos[contact_index] - tip_pos[contact_index:]
+        self.force_rep = np.zeros(time.size)
+        self.force_rep[contact_index:] = force[contact_index:] - force[contact_index]
+
+    def save(self, savename):
+        if self.force is None:
+            print('nothing has been integrated, saving will be empty!')
+        saveroot = '/home/mmccraw/Desktop/gwu/projects/repulsive contact/sim data/'
+        df = pd.DataFrame({'time': self.time, 'separation': self.separation, 'tip_pos': self.tip_pos,
+                           'inden': self.inden, 'force_rep': self.force_rep})
+        data = {'df': df, 'v0': self.v0, 'viscoelastic': self.viscoelastic, 'R': self.R, 'dr': self.dr,
+                'nr': self.nr, 'E0': self.E0, 'Einf': self.Einf, 'Tau': self.Tau}
+        with open(next_path(saveroot + savename + '-%s.sim'), 'wb') as f:
+            pickle.dump(data)
