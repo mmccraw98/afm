d7b8abdf27d38edc57d16fa7ef4f6a4eb92fd303
admin <mmccraw98@gwu.edu>
2023-01-21 13:06:46 -0500

    added individual control over attractive and repulsive vdW potential

--- afm/sim.py
+++ afm/sim.py
@@ -73,13 +73,51 @@ def p_vdw(h, H1=1e-19, H2=1e-19, z0=1e-9):
     '''
     calculates the van der Waals pressure and derivative as a function of distance
     :param h: (float or numpy array) distance between bodies
-    :param H: (float) Hamaker constant
+    :param H1: (float) repulsive Hamaker constant
+    :param H2: (float) attractive Hamaker constant
     :param z0: (float) equilibrium separation distance
     :return: (float or numpy array x2) pressure, derivative of pressure with respect to distance
     '''
     return (1 / (6 * np.pi * h ** 3) * (H1 * (z0 / h) ** 6 - H2),
             1 / (2 * np.pi * h ** 4) * (H2 - 3 * H1 * (z0 / h) ** 6))
 
+def p_exp_approx(h, H_rep=1e7, k_inv=1/1e-9, h_off=0):
+    '''
+    calculates an approximation for an exponential repulsion potential
+    :param h: float distance between bodies
+    :param H_rep: float repulsive Hamaker constant
+    :param k_inv: float debye length
+    :param h_off: float offset shift to distance
+    :return: float pressure and pressure derivative
+    '''
+    # P, Ph
+    return (H_rep / (h_off + k_inv * h),
+            - H_rep * k_inv / (h_off + k_inv * h) ** 2)
+
+def p_exp(h, magnitude=1e7, decay_length=1e-9):
+    '''
+    calculates an exponential repulsive potential distribution (cannot generally be used on its own)
+    :param h: float distance between bodies
+    :param magnitude: float magnitude of repulsive force at 0 separation
+    :param decay_length: float decay constant
+    :return: float pressure and pressure derivative
+    '''
+    return (magnitude * np.exp(- h / decay_length),
+            - magnitude / decay_length * np.exp(- h / decay_length))
+def p_exp_and_rep(h, magnitude=1e7, decay_length=1e-9, H_rep=1e-19, z0=0.5e-9):
+    '''
+    superposition of exponential repulsion and short range, hard wall repulsion
+    :param h: float distance between bodies
+    :param magnitude: float magnitude of exponential repulsive force at 0 separation
+    :param decay_length: float decay constant of exponential force
+    :param H_rep: float hard wall repulsion hamaker constant
+    :param z0: decay constant of hard wall repulsion
+    :return: float pressure and pressure derivative
+    '''
+    p1, ph1 = p_exp(h, magnitude, decay_length)
+    p2, ph2 = p_vdw(h, H2=0, H1=H_rep, z0=z0)
+    return (p1 + p2,
+            ph1 + ph2)
 
 def rk4(state, dt, rhs_func, *args):
     '''
@@ -96,125 +134,194 @@ def rk4(state, dt, rhs_func, *args):
     k4 = rhs_func(state + dt * k3, *args)
     return state + (k1 + 2 * k2 + 2 * k3 + k4) * dt / 6
 
+def rhs_N_1_rigid(state, r, dr, R, k_ij, I, v0, b1, b0, c1, c0, p_func, *args):
+    '''
+    calculates the derivative of the state vector (surface deformation and probe position) assuming that the
+    probe position is prescribed by a time dependent velocity v0(t) (i.e. by a rigid cantilever)
+    also assuming that the viscoelastic ODE is order N=1
+    :param state: numpy array state vector (deformation, probe position)
+    :param r: float radial spatial axis
+    :param dr: float spatial discretization
+    :param R: float spherical probe radius
+    :param k_ij: numpy matrix (nr x nr) integrating kernel
+    :param I: numpy matrix (nr x nr) identity matrix
+    :param v0: float time dependent prescribed velocity of the probe
+    :param b1: float viscoelastic coefficient b1
+    :param b0: float viscoelastic coefficient b0
+    :param c1: float viscoelastic coefficient c1
+    :param c0: float viscoelastic coefficient c0
+    :param p_func: function for calculating sphere-point pressure distribution p_func(h, *args)
+    :param args: optional arguments for p_func
+    :return: numpy array derivative of state vector (deformation rate, probe velocity)
+    '''
+    u, h0 = state  # unpack the state vector
+    h = h_calc(h0, u, f_sphere, r, R)  # calculate the separation
+    p, p_h = p_func(h, *args)  # calculate the pressure and derivative
+    # calculate LHS G matrix
+    G_ij = c1 * dr * k_ij * p_h * r - b1 * I
+    # calculate RHS vectors, d_j
+    d_j = c1 * v0 * dr * (p_h * r) @ k_ij + c0 * dr * (p * r) @ k_ij + b0 * u
+    u_dot = np.linalg.solve(G_ij, d_j)  # solve the system of equations
+    return np.array([u_dot, v0], dtype='object')
 
-class sim_rajabifar_1():
-    def __init__(self, v0, h0, R, nr=1000, dr=1.5e-9, dt=1e-4, nt=int(1e6)):
-        '''
-        create an instance of Rajabifar's 'exact' approach to solving the Attard equation as outlined here:
-        Dynamic AFM on Viscoelastic Polymer Samples with Surface Forces - Bahram Rajabifar
-        :param v0: (float) directional velocity of the indenter (indenter follows linear path in time) (m/s)
-        :param h0: (float) initial position of indenter (m)
-        :param R: (float) radius of indenter (m)
-        :param nr: (int) number of points in the radial discretization
-        :param dr: (float) radial discretization (m)
-        :param dt: (float) temporal discretization (s)
-        :param nt: (int) number of points in the temporal discretization
-        '''
-        self.v0 = v0
-        self.h0 = h0
-        self.R = R
-        self.nr = nr
-        self.dr = dr
-        self.dt = dt
-        self.nt = nt
-        # initialize
-        self.r = np.linspace(1, self.nr, self.nr) * self.dr
-        self.k_ij = np.array([K(r_, self.r, self.dr) for r_ in self.r])  # calculate integrating kernel
-        self.I = np.eye(self.r.size)  # identity matrix
-        self.force = None
-        self.separation = None
-        self.tip_pos = None
-        self.time = None
-        self.inden = None
-        self.force_rep = None
-        self.viscoelastic = None
-        self.E0 = None
-        self.Einf = None
-        self.Tau = None
-        self.H1 = None
-        self.H2 = None
-        self.z0 = None
+def get_coefs_rajabifar(Gg, Ge, Tau, v):
+    '''
+    get viscoelastic ODE coefficients in the style of Rajabifar and Attard
+    :param Gg: float instantaneous shear modulus
+    :param Ge: float equilibrium shear modulus
+    :param Tau: float relaxation time
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return 1, 1 / Tau, (1 - v ** 2) / Gg, (1 - v ** 2) / (Ge * Tau)
 
-    def rhs_vdw_sls(self, state, E0, Einf, Tau, viscoelastic):
-        '''
-        calculate the time derivative of the state vector (u, h0) assuming that h0(t)=v0*t
-        works via inversion of J_ij * u_dot_i = b_i
-        :param state: (numpy array) state vector (u sample deformation, h0 indenter position)
-        :return: (numpy array) derivative of state vector (u_dot, v0)
-        '''
-        u, h0 = state  # unpack state vector
-        h = h_calc(h0, u, f_sphere, self.r, self.R)  # calculate the separation for the spherical indenter
-        p, p_h = p_vdw(h, H1=self.H1, H2=self.H2, z0=self.z0)  # calculate the vdW pressure
-        J_ij = 1 / E0 * self.k_ij * self.dr * p_h * self.r - self.I  # calculate the LHS matrix J_ij
-        b_i = 1 / E0 * self.dr * self.v0 * (p_h * self.r) @ self.k_ij  # calculate the RHS vector b_i
-        if viscoelastic:  # introduce viscoelastic relaxation of SLS material
-            u_inf = - 1 / Einf * self.dr * (p * self.r) @ self.k_ij  # skip if elastic
-            b_i += 1 / Tau * (u - u_inf)  # skip if elastic
-        u_dot = np.linalg.solve(J_ij, b_i)  # invert governing matrix eqn to find u_dot_i directly
-        return np.array([u_dot, self.v0], dtype='object')
+def get_coefs_sls(Gg, Ge, Tau, v):
+    '''
+    get viscoelastic ODE coefficients for a standard linear solid (SLS) material
+    :param Gg: flaot instantaneous shear modulus
+    :param Ge: float equilibrium shear modulus
+    :param Tau: float relaxation time
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return Gg * Tau, Ge, (1 - v ** 2) * Tau, (1 - v ** 2)
 
-    def simulate(self, E0, Einf, Tau, viscoelastic, force_target, depth_ratio_target=0.1, H1=1e-19, H2=1e-19, z0=1e-9):
-        '''
-        :param E0: (float) normalized instantaneous modulus (Pa) E0 / (1 - v^2)
-        :param Einf: (float) normalized equilibrium modulus (Pa) Einf / (1 - v^2)
-        :param Tau: (float) relaxation time (s)
-        :param viscoelastic: (bool) whether sample is viscoelastic (SLS) or elastic (w/ modulus E0)
-        :param force_target: (float) target force (N)
-        :param depth_ratio_target: (float) ratio of indenter radius to indent into sample
-        :return:
-        '''
-        pos_target = - depth_ratio_target * self.R  # calculate position target for iteration
+def get_coefs_elastic(G, v):
+    '''
+    get viscoelastic ODE coefficients for an elastic solid
+    :param G: float shear modulus
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return G, 0, (1 - v ** 2), 0
 
-        # set loggers
-        self.force = np.zeros(self.nt)
-        self.separation = np.zeros(self.nt)
-        self.tip_pos = np.zeros(self.nt)
-        self.time = np.zeros(self.nt)
-        self.viscoelastic = viscoelastic
-        self.E0 = E0
-        self.Einf = Einf
-        self.Tau = Tau
-        self.H1 = H1
-        self.H2 = H2
-        self.z0 = z0
+def get_coefs_viscous(mu, v):
+    '''
+    get viscoelastic ODE coefficients for a viscous solid
+    :param mu: float viscosity
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return mu, 0, 0, (1 - v ** 2)
 
-        h0 = self.h0
-        u = np.zeros(self.r.shape)
-        state = np.array([u, h0], dtype='object')
-        print(' % |  z_tip  |  z_target |  force  |  force_target')
-        for n in range(self.nt):
-            state = rk4(state, self.dt, self.rhs_vdw_sls, E0, Einf, Tau, viscoelastic)
-            u, h0 = state
-            h = h_calc(h0, u, f_sphere, self.r, self.R)
-            p, p_h = p_vdw(h, H1=self.H1, H2=self.H2, z0=self.z0)
-            self.force[n] = (2 * np.pi * p @ self.r * self.dr)
-            self.separation[n] = h[0]
-            self.tip_pos[n] = h0
-            self.time[n] = n * self.dt
-            if n % (0.01 * self.nt) == 0:
-                print('{} | {:.1f} nm | {:.1f} nm | {:.1f} nN | {:.1f} nN'.format(n / self.nt,
-                                                                                  self.tip_pos[n] * 1e9, pos_target * 1e9,
-                                                                                  self.force[n] * 1e9, force_target * 1e9))
-            if self.force[n] > force_target or self.tip_pos[n] < pos_target:
-                self.force = self.force[:n]
-                self.separation = self.separation[:n]
-                self.tip_pos = self.tip_pos[:n]
-                self.time = self.time[:n]
-                break
-        print('done!')
-        self.inden = np.zeros(self.time.size)
-        contact_index = np.argmin(self.force)
-        self.inden[contact_index:] = self.tip_pos[contact_index] - self.tip_pos[contact_index:]
-        self.force_rep = np.zeros(self.time.size)
-        self.force_rep[contact_index:] = self.force[contact_index:] - self.force[contact_index]
+def get_coefs_kvf(J, Tau, v):
+    '''
+    get viscoelastic ODE coefficients for a kelvin-voigt viscous solid (fluid)
+    :param J: float shear compliance
+    :param Tau: float retardation time
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return Tau, 1, 0, (1 - v ** 2) * J
 
-    def save(self, savename):
-        if self.force is None:
-            print('nothing has been integrated, saving will be empty!')
-        saveroot = '/home/mmccraw/Desktop/gwu/projects/repulsive contact/sim data/'
-        df = pd.DataFrame({'time': self.time, 'separation': self.separation, 'tip_pos': self.tip_pos,
-                           'inden': self.inden, 'force_rep': self.force_rep})
-        data = {'df': df, 'v0': self.v0, 'viscoelastic': self.viscoelastic, 'R': self.R, 'dr': self.dr,
-                'nr': self.nr, 'E0': self.E0, 'Einf': self.Einf, 'Tau': self.Tau}
-        with open(next_path(saveroot + savename + '-%s.sim'), 'wb') as f:
-            pickle.dump(data, f)
+def get_coefs_mf(G, Tau, v):
+    '''
+    get viscoelastic ODE coefficients for a maxwell viscous solid (fluid)
+    :param G: float shear modulus
+    :param Tau: float relaxation time
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return G * Tau, 0, (1 - v ** 2) * Tau, (1 - v ** 2)
+
+def get_coefs_gkv(Jg, Je, Tau, v):
+    '''
+    get viscoelastic ODE coefficients for (generalized) kelvin-voigt solid
+    :param Jg: float instantaneous shear compliance
+    :param Je: float equilibrium shear compliance
+    :param Tau: float retardation time
+    :param v: float poisson's ratio
+    :return: b1, b0, c1, c0
+    '''
+    return Tau, 1, (1 - v ** 2) * Jg, (1 - v ** 2) * Je
+
+
+def simulate_rigid_N1(Gg, Ge, Tau, v, v0, h0, R, p_func, *args,
+             nr=int(1e3), dr=1.5e-9,
+             dt=1e-4, nt=int(1e6),
+             force_target=1e-6, Zr_target=-0.1):
+    '''
+    integrate the interaction of the probe (connected to a rigid cantilever) with a sample defined by a viscoelastic
+    ODE of maximum order N=1, using RK4 time integration
+    :param Gg: float instantaneous shear modulus
+    :param Ge: float equilibrium shear modulus
+    :param Tau: float relaxation time
+    :param v: float poisson's ratio
+    :param v0: float prescribed probe velocity
+    :param h0: float initial position of the probe relative to the sample
+    :param R: float radius of the spherical probe
+    :param p_func: function describing the point-probe pressure distribution p_func(h, *args)
+    :param args: optional arguments for p_func pressure distribution
+    :param nr: int number of spatial discretization points
+    :param dr: float spatial discretization
+    :param dt: float temporal discretization
+    :param nt: int number of time discretization points
+    :param force_target: float maximum force in simulation
+    :param Zr_target: float target position of probe as a ratio of the probe radius
+    :return: data dict containing sim dataframe and sim parameters
+    '''
+    saved_args = locals()  # save all function arguments for later
+    # discretize domain
+    r = np.linspace(1, nr, nr) * dr
+    # calculate integrating kernels
+    k_ij = np.array([K(r_, r, dr) for r_ in r])
+    I = np.eye(r.size)  # make identity matrix
+    u = np.zeros(r.shape)  # initialize deformation
+    state = np.array([u, h0], dtype='object')  # make state vector
+
+    # get generalized viscoelastic coefficients
+    if Ge == 0 and Tau == 0:
+        b1, b0, c1, c0 = get_coefs_elastic(Gg, v)
+    else:
+        b1, b0, c1, c0 = get_coefs_sls(Gg, Ge, Tau, v)
+
+    print('b1 | b0 | c1 | c0')
+    print(b1, b0, c1, c0)
+
+    # calculate exact position target
+    pos_target = R * Zr_target
+
+    # make loggers
+    force = np.zeros(nt)
+    separation = np.zeros(nt)
+    tip_pos = np.zeros(nt)
+    time = np.zeros(nt)
+
+    # run simulation
+    print(' % |  z_tip  |  z_target |  force  |  force_target  |  u(inf,t)')
+    for n in range(nt):
+        # update the state according to rk4 integration
+        state = rk4(state, dt, rhs_N_1_rigid, r, dr, R, k_ij, I, v0, b1, b0, c1, c0, p_func, *args)
+        u, h0 = state  # get the state variables
+        h = h_calc(h0, u, f_sphere, r, R)  # calculate the separation between the probe and the sample
+        p, p_h = p_func(h, *args)  # calculate the pressure distribution
+        force[n] = (2 * np.pi * p @ r * dr)  # calculate the force
+        # log
+        separation[n] = h[0]
+        tip_pos[n] = h0
+        time[n] = n * dt
+        if n % (0.001 * nt) == 0:
+            print('{} | {:.1f} nm | {:.1f} nm | {:.1f} nN | {:.1f} nN | {:.1f} nm'.format(
+                n / nt, tip_pos[n] * 1e9, pos_target * 1e9, force[n] * 1e9,
+                force_target * 1e9, u[-1] * 1e9))
+        # early exit conditions
+        if force[n] > force_target or tip_pos[n] < pos_target:
+            print('force and target ', force[n], force_target)
+            print('pos and target ', tip_pos[n], pos_target)
+            force = force[:n + 1]
+            separation = separation[:n + 1]
+            tip_pos = tip_pos[:n + 1]
+            time = time[:n + 1]
+            break
+    print('done!')
+    # save sim data
+    inden = np.zeros(time.size)
+    contact_index = np.argmin(force)
+    inden[contact_index:] = tip_pos[contact_index] - tip_pos[contact_index:]
+    force_rep = np.zeros(time.size)
+    force_rep[contact_index:] = force[contact_index:] - force[contact_index]
+    df = pd.DataFrame({'time': time, 'separation': separation, 'tip_pos': tip_pos,
+                       'force': force, 'inden': inden, 'force_rep': force_rep,
+                       'deformation': separation - tip_pos})
+    data = {'df': df, 'sim_arguments': saved_args}
+    return data
