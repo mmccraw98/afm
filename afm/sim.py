31186d2f4466d242c8c55f4367efa267edf0c119
mmccraw <mmccraw98@gmail.com>
2023-02-16 19:30:22 -0500

    added adjustable starting point

--- afm/sim.py
+++ afm/sim.py
@@ -557,9 +557,9 @@ def simulate_rigid_N1(Gg, Ge, Tau, v, v0, h0, R, p_func, *args,
         print('adjusting starting point...')
         # try first-step
         f = np.inf
-        increment = 1e-8
+        increment_factor = 10
         while f > 1e-10:  # while starting force is too large, increase h0
-            h0 += increment
+            h0 *= increment_factor
             if use_cuda:  # need to benchmark this
                 state = torch.cat((u, h0), 1)
                 state, (p, h) = rk4(state, dt, rhs_N_1_rigid_cuda, r, dr, R, k_ij, I, v0, b1, b0, c1, c0, p_func, *args)
